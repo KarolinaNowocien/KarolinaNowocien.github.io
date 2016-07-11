1573101d55ea2ad84ae48fb1c5ef730b37f5bc91
KarolinaNowocień <karo.nowocien@gmail.com>
2016-07-11 16:58:55 +0200

    added all js files

---
+++ sites/micherabeauty2/js/main.js
@@ -0,0 +1,168 @@
+/*
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+(function($) {
+
+	skel.breakpoints({
+		wide: '(max-width: 1680px)',
+		normal: '(max-width: 1280px)',
+		narrow: '(max-width: 980px)',
+		narrower: '(max-width: 840px)',
+		mobile: '(max-width: 736px)'
+	});
+
+	$(function() {
+
+		var	$window = $(window),
+			$body = $('body'),
+			$header = $('#header'),
+			$banner = $('#banner');
+			$banner2 = $('#banner2');
+
+		// Disable animations/transitions until the page has loaded.
+			$body.addClass('is-loading');
+
+			$window.on('load', function() {
+				$body.removeClass('is-loading');
+			});
+
+		// CSS polyfills (IE<9).
+			if (skel.vars.IEVersion < 9)
+				$(':last-child').addClass('last-child');
+
+		// Fix: Placeholder polyfill.
+			$('form').placeholder();
+
+		// Prioritize "important" elements on narrower.
+			skel.on('+narrower -narrower', function() {
+				$.prioritize(
+					'.important\\28 narrower\\29',
+					skel.breakpoint('narrower').active
+				);
+			});
+
+		// Scrolly links.
+			$('.scrolly').scrolly({
+				speed: 1000,
+				offset: -10
+			});
+
+		// Dropdowns.
+			$('#nav > ul').dropotron({
+				mode: 'fade',
+				noOpenerFade: true,
+				expandMode: (skel.vars.touch ? 'click' : 'hover')
+			});
+
+		// Off-Canvas Navigation.
+
+			// Navigation Button.
+				$(
+					'<div id="navButton">' +
+						'<a href="#navPanel" class="toggle"></a>' +
+					'</div>'
+				)
+					.appendTo($body);
+
+			// Navigation Panel.
+				$(
+					'<div id="navPanel">' +
+						'<nav>' +
+							$('#nav').navList() +
+						'</nav>' +
+					'</div>'
+				)
+					.appendTo($body)
+					.panel({
+						delay: 500,
+						hideOnClick: true,
+						hideOnSwipe: true,
+						resetScroll: true,
+						resetForms: true,
+						side: 'left',
+						target: $body,
+						visibleClass: 'navPanel-visible'
+					});
+
+			// Fix: Remove navPanel transitions on WP<10 (poor/buggy performance).
+				if (skel.vars.os == 'wp' && skel.vars.osVersion < 10)
+					$('#navButton, #navPanel, #page-wrapper')
+						.css('transition', 'none');
+
+		// Header.
+		// If the header is using "alt" styling and #banner is present, use scrollwatch
+		// to revert it back to normal styling once the user scrolls past the banner.
+		// Note: This is disabled on mobile devices.
+			if (!skel.vars.mobile
+			&&	$header.hasClass('alt')
+			&&	$banner.length > 0) {
+
+				$window.on('load', function() {
+
+					$banner.scrollwatch({
+						delay:		0,
+						range:		1,
+						anchor:		'top',
+						on:			function() { $header.addClass('alt reveal'); },
+						off:		function() { $header.removeClass('alt'); }
+					});
+
+				});
+
+			}
+
+			if (!skel.vars.mobile
+			&&	$header.hasClass('alt')
+			&&	$banner2.length > 0) {
+
+				$window.on('load', function() {
+
+					$banner2.scrollwatch({
+						delay:		0,
+						range:		1,
+						anchor:		'top',
+						on:			function() { $header.addClass('alt reveal'); },
+						off:		function() { $header.removeClass('alt'); }
+					});
+
+				});
+
+			}
+
+					var $el, $ps, $up, totalHeight;
+
+		$(".sidebar-box .button--simple").click(function() {
+
+		  totalHeight = 0
+
+		  $el = $(this);
+		  $p  = $el.parent();
+		  $up = $p.parent();
+		  $ps = $up.find("p:not('.read-more')");
+
+		  // measure how tall inside should be by adding together heights of all inside paragraphs (except read-more paragraph)
+		  $ps.each(function() {
+		    totalHeight += $(this).outerHeight();
+		  });
+
+		  $up
+		    .css({
+		      // Set height to prevent instant jumpdown when max height is removed
+		      "height": $up.height(),
+		      "max-height": 9999
+		    })
+		    .animate({
+		      "height": totalHeight
+		    });
+
+		  // fade out read-more
+		  $p.fadeOut();
+
+		  // prevent jump-down
+		  return false;
+
+		});
+
+	});
+
+})(jQuery);
